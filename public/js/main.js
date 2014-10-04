8473369a2f42af032940f12b83d62fd6286b5c0b
Jared Seehafer <jared@seehafer.net>
2014-10-04 16:36:55 -0700

    working thru multiple filters

--- public/js/main.js
+++ public/js/main.js
@@ -6,10 +6,37 @@ $(window).load( function() {
     layoutMode: 'masonry',
   });
   
-  $('#size-filter').on( 'click', 'button', function() {
+  var filters = {};
+  
+ /* $('#size-filter').on( 'click', 'button', function() {
     var filterValue = $( this ).attr('data-filter');
     $container.isotope({ filter: filterValue });
   });
+  
+  $('#color-filter').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+  }); */
+  
+  $('.container').on( 'click', 'button', function() {
+    var $this = $(this);
+    // get group key
+    var $buttonGroup = $this.parents('.btn-group-vertical');
+    var filterGroup = $buttonGroup.attr('id');
+    // set filter for group
+    filters[ filterGroup ] = $this.attr('data-filter');
+    // combine filters
+    var filterValue = '';
+    var i = 0;
+    for ( var prop in filters ) {
+      if (i > 0 && i != filters.length-1) filterValue += ",";
+      filterValue += filters[ prop ];
+      i++;
+    }
+    // set filter for Isotope
+    $container.isotope({ filter: filterValue });
+  });
+  
   
   // change active class on buttons
   $('.btn-group-vertical').each( function( i, buttonGroup ) {
