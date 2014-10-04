d39b7d253fc976417c4b7d6bc2ac1349be5ab03d
David Crawford <david.crawford@gmail.com>
2014-10-04 13:22:08 -0700

    Allow reset option in fixture task

--- Gruntfile.js
+++ Gruntfile.js
@@ -63,8 +63,9 @@ module.exports = function (grunt) {
   var MongoClient = require('mongodb').MongoClient,
     fs = require('fs');
 
-  grunt.registerTask('fixture', 'Load data from fixture file', function (name) {
+  grunt.registerTask('fixture', 'Load data from fixture file', function (name, reset) {
     var done = this.async();
+    reset = reset === "reset";
     console.log('Connecting...');
     MongoClient.connect('mongodb://127.0.0.1:27017/pocketdoug-development', function (err, db) {
       var collection = db.collection(name);
@@ -72,16 +73,25 @@ module.exports = function (grunt) {
         encoding: 'utf-8'
       });
       var data = JSON.parse(data_str);
-      console.log('Inserting...');
-      console.log(data);
-      collection.insert(data, {w: 1}, function (err, objects) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log("Inserted " + objects.length + " objects.");
-        }
-        done();
-      });
+      var insert = function () {
+        console.log('Inserting ' + data.length + ' objects...');
+        collection.insert(data, {w: 1}, function (err, objects) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("Inserted " + objects.length + " objects.");
+          }
+          done();
+        });
+      }
+
+      if (reset) {
+        console.log()
+        collection.drop(insert);
+      } else {
+        insert();
+      }
+
     })
   });
 };
