578b96d836ea2be1b29fd3c91f968731c0298440
Jared Seehafer <jared@seehafer.net>
2014-10-04 15:47:41 -0700

    first pass on filters

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,25 @@
 console.log(Isotope);
-$( function() {
+$(window).load( function() {
   // init Isotope
   var $container = $('#items').isotope({
     itemSelector: '.bird',
     layoutMode: 'masonry',
   });
+  
+  $('#size-filter').on( 'click', 'button', function() {
+    var filterValue = $( this ).attr('data-filter');
+    $container.isotope({ filter: filterValue });
+  });
+  
+  // change active class on buttons
+  $('.btn-group-vertical').each( function( i, buttonGroup ) {
+    var $buttonGroup = $( buttonGroup );
+    $buttonGroup.on( 'click', 'button', function() {
+      $buttonGroup.find('.active').removeClass('active');
+      $( this ).addClass('active');
+    });
+  });
+  
 
   var Bird = Backbone.Model.extend();
   Bird.Collection = Backbone.Collection.extend({
