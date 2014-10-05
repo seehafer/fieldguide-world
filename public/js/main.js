a6277c1488709c8d21e806cd8ec9874b6f465165
David Crawford <david.crawford@gmail.com>
2014-10-04 19:13:50 -0700

    Remove stray code

--- public/js/main.js
+++ public/js/main.js
@@ -1,4 +1,3 @@
-console.log(Isotope);
 $(window).load( function() {
   // init Isotope
   var $container = $('#items').isotope({
@@ -46,9 +45,9 @@ $(window).load( function() {
   });
 
   $('img').capty({ animation: 'fade', speed: 100});
-  
+
   $('.bird_link').popover({container: 'body'}); // fun-facts
-  
+
   $('body').on('click', 'div.bird', function (e) {
     window.location.href = '/edit/' + $(e.currentTarget).data('id') + '/';
   })
